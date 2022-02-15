0f4ed93818c071da76af13ff059f3467fe1874ed
Elvis <elvis.sebatane@gmail.com>
2022-02-15 14:49:03 +0200

    Done

--- accountApi/adminView/urls.py
+++ accountApi/adminView/urls.py
@@ -5,6 +5,6 @@ app_name = 'adminView'
 
 urlpatterns = [
     path('', views.index, name='index'),
-    #path('repos/', views.index, name='index'),
-    path('<int:pk>/', views.detail, name='detail'),
+    path('<int:pk>/', views.csvReport, name='csv'),
+    path('detail/', views.detail, name='detail'),
 ]
